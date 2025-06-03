fdce800f968b57bfdfdb8e5f9e6076fab86d96c5
Mariano Bessone <marianobessone2003@gmail.com>
2025-06-03 19:17:49 -0300

    Agregar archivos de proyecto.

---
+++ TPRegistroGastos/Controllers/GastoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TPRegistroGastos.Models;
+
+namespace TPRegistroGastos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GastoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GastoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Gasto>>> Get()
+        {
+            return await _context.Gastos.OrderByDescending(g => g.Fecha).ToListAsync();
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Gasto>> Post(Gasto gasto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Gastos.Add(gasto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = gasto.Id }, gasto);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ TPRegistroGastos/Models/Gasto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPRegistroGastos.Models
+{
+    public class Gasto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El monto es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor a cero.")]
+        public decimal Monto { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [StringLength(250, MinimumLength = 3, ErrorMessage = "La descripción debe tener entre 3 y 250 caracteres.")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "El nombre del comercio es obligatorio.")]
+        [StringLength(250, ErrorMessage = "El nombre del comercio no debe superar los 250 caracteres.")]
+        public string NombreComercio { get; set; }
+    }
+}
